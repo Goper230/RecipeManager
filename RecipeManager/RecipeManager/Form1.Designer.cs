2b3f434c9b7988676005117a3280a27c597f6423
Goper230 <92451318+Goper230@users.noreply.github.com>
2024-05-31 07:51:33 +0700

    Add files via upload

---
+++ RecipeManager/RecipeManager/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace RecipeManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.listBoxRecipes = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listBoxChoosen = new System.Windows.Forms.ListBox();
+            this.listBoxDouble = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.MoveButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(8, 8);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1006, 639);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonDelete);
+            this.tabPage1.Controls.Add(this.buttonCreate);
+            this.tabPage1.Controls.Add(this.buttonOpen);
+            this.tabPage1.Controls.Add(this.listBoxRecipes);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(998, 613);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Создание и Открытие Рецептов";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDelete.Location = new System.Drawing.Point(791, 175);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(201, 77);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCreate.Location = new System.Drawing.Point(791, 92);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(201, 77);
+            this.buttonCreate.TabIndex = 2;
+            this.buttonCreate.Text = "Создать";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpen.Location = new System.Drawing.Point(791, 9);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(201, 77);
+            this.buttonOpen.TabIndex = 1;
+            this.buttonOpen.Text = "Открыть";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // listBoxRecipes
+            // 
+            this.listBoxRecipes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxRecipes.FormattingEnabled = true;
+            this.listBoxRecipes.Location = new System.Drawing.Point(9, 9);
+            this.listBoxRecipes.Name = "listBoxRecipes";
+            this.listBoxRecipes.Size = new System.Drawing.Size(776, 576);
+            this.listBoxRecipes.TabIndex = 0;
+            this.listBoxRecipes.SelectedIndexChanged += new System.EventHandler(this.listBoxRecipes_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.DeleteButton);
+            this.tabPage2.Controls.Add(this.MoveButton);
+            this.tabPage2.Controls.Add(this.listBox3);
+            this.tabPage2.Controls.Add(this.listBoxDouble);
+            this.tabPage2.Controls.Add(this.listBoxChoosen);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(998, 613);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Выбор рецептов для готовки";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listBoxChoosen
+            // 
+            this.listBoxChoosen.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxChoosen.FormattingEnabled = true;
+            this.listBoxChoosen.Location = new System.Drawing.Point(0, 125);
+            this.listBoxChoosen.Name = "listBoxChoosen";
+            this.listBoxChoosen.Size = new System.Drawing.Size(294, 485);
+            this.listBoxChoosen.TabIndex = 0;
+            // 
+            // listBoxDouble
+            // 
+            this.listBoxDouble.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.listBoxDouble.FormattingEnabled = true;
+            this.listBoxDouble.Location = new System.Drawing.Point(356, 125);
+            this.listBoxDouble.Name = "listBoxDouble";
+            this.listBoxDouble.Size = new System.Drawing.Size(294, 485);
+            this.listBoxDouble.TabIndex = 1;
+            this.listBoxDouble.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(704, 125);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(294, 485);
+            this.listBox3.TabIndex = 2;
+            // 
+            // MoveButton
+            // 
+            this.MoveButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.MoveButton.Location = new System.Drawing.Point(400, 79);
+            this.MoveButton.Name = "MoveButton";
+            this.MoveButton.Size = new System.Drawing.Size(206, 40);
+            this.MoveButton.TabIndex = 3;
+            this.MoveButton.Text = "Добавить";
+            this.MoveButton.UseVisualStyleBackColor = true;
+            this.MoveButton.Click += new System.EventHandler(this.MoveButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(34, 79);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(201, 40);
+            this.DeleteButton.TabIndex = 4;
+            this.DeleteButton.Text = "Удалить";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(998, 613);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Выбор Покупки";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 655);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.ListBox listBoxRecipes;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBoxDouble;
+        private System.Windows.Forms.ListBox listBoxChoosen;
+        private System.Windows.Forms.Button MoveButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
---
+++ RecipeManager/RecipeManager/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecipeManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //tabPage1.Dock = DockStyle.Fill;
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            // Открытие выделенного рецепта
+            if (listBoxRecipes.SelectedItem != null)
+            {
+                string selectedRecipe = listBoxRecipes.SelectedItem.ToString();
+                MessageBox.Show($"Открытие рецепта: {selectedRecipe}", "Рецепт", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Создаем новую вкладку
+                TabPage tabPage = new TabPage(selectedRecipe);
+
+                // Добавляем TextBox для cook
+                TextBox cook = new TextBox();
+                cook.Multiline = true;
+                cook.Location = new System.Drawing.Point(64, 123);
+                cook.Size = new System.Drawing.Size(464, 490);
+                cook.Anchor = AnchorStyles.Bottom | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left;
+
+
+                // Добавляем TextBox для ingredients
+                TextBox ingredients = new TextBox();
+                ingredients.Multiline = true;
+                ingredients.Location = new System.Drawing.Point(702, 41);
+                ingredients.Size = new System.Drawing.Size(296, 576);
+                ingredients.Anchor = AnchorStyles.Bottom | AnchorStyles.Top | AnchorStyles.Right;
+
+                // Добавляем TextBox для recipeName
+                TextBox recipeName = new TextBox();
+                recipeName.Multiline = false;
+                recipeName.Location = new System.Drawing.Point(240, 30);
+                recipeName.Size = new System.Drawing.Size(381, 37);
+                recipeName.Anchor = AnchorStyles.Top;
+
+                // Создаем и настраиваем Label для cook
+                Label NameLabel = new Label();
+                NameLabel.Text = "Как готовить";
+                NameLabel.AutoSize = true; // Чтобы метка автоматически подстраивалась под текст
+                NameLabel.Font = new Font(NameLabel.Font.FontFamily, 14, FontStyle.Bold);
+                NameLabel.Anchor = AnchorStyles.Top;
+                // Рассчитываем позицию Label над TextBox cook по центру
+                int labelX = cook.Location.X + (cook.Width - NameLabel.PreferredWidth) / 2;
+                int labelY = cook.Location.Y - NameLabel.PreferredHeight - 10; // Немного выше TextBox cook
+                NameLabel.Location = new System.Drawing.Point(labelX, labelY);
+
+
+                // Создаем и настраиваем Label для ingredients
+                Label IngredientsLabel = new Label();
+                IngredientsLabel.Text = "Ингредиенты";
+                IngredientsLabel.AutoSize = true; // Чтобы метка автоматически подстраивалась под текст
+                IngredientsLabel.Font = new Font(IngredientsLabel.Font.FontFamily, 14, FontStyle.Bold);
+                IngredientsLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+                // Рассчитываем позицию Label над TextBox ingredients по центру
+                int ingredientsLabelX = ingredients.Location.X + (ingredients.Width - IngredientsLabel.PreferredWidth) / 2;
+                int ingredientsLabelY = ingredients.Location.Y - IngredientsLabel.PreferredHeight - 10; // Немного выше TextBox ingredients
+                IngredientsLabel.Location = new System.Drawing.Point(ingredientsLabelX, ingredientsLabelY);
+
+
+                // Добавляем новую вкладку в TabControl
+                tabControl1.TabPages.Add(tabPage);
+                tabControl1.SelectedTab = tabPage; // Переключаемся на новую вкладку
+                tabPage.Controls.Add(IngredientsLabel);
+                tabPage.Controls.Add(NameLabel);
+                tabPage.Controls.Add(recipeName);
+                tabPage.Controls.Add(ingredients);
+                tabPage.Controls.Add(cook);
+            }   
+            else
+            {
+                MessageBox.Show("Выберите рецепт для открытия", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            // Пример добавления рецепта
+            string newRecipe = "Новый рецепт " + (listBoxRecipes.Items.Count + 1);
+            listBoxRecipes.Items.Add(newRecipe);
+            DuplicateListBoxItems();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            // Удаление выделенного рецепта
+            if (listBoxRecipes.SelectedItem != null)
+            {
+                listBoxRecipes.Items.Remove(listBoxRecipes.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Выберите рецепт для удаления", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        
+
+        private void listBoxRecipes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void DuplicateListBoxItems()
+        {
+            listBoxDouble.Items.Clear();
+            foreach (var item in listBoxRecipes.Items)
+            {
+                listBoxDouble.Items.Add(item);
+            }
+        }
+
+        private void MoveButton_Click(object sender, EventArgs e)
+        {
+            foreach (var item in listBoxDouble.SelectedItems)
+            {
+                listBoxChoosen.Items.Add(item);
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            while (listBoxChoosen.SelectedItems.Count > 0)
+            {
+                listBoxChoosen.Items.Remove(listBoxChoosen.SelectedItems[0]);
+            }
+        }
+    }
+        
+}
